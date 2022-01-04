0033aea3d15f4e8ac5130c961d8ece5d9f4d26a6
Eugen <eugen.mllr@gmail.com>
2022-01-04 15:45:23 +0100

    Update request.rb

--- lib/turnout/request.rb
+++ lib/turnout/request.rb
@@ -7,12 +7,18 @@ module Turnout
     end
 
     def allowed?(settings)
-      path_allowed?(settings.allowed_paths) || ip_allowed?(settings.allowed_ips)
+      path_allowed?(settings.allowed_paths) || ip_allowed?(settings.allowed_ips) || subdomain_allowed?(settings.allowed_subdomains)
     end
 
     private
 
     attr_reader :rack_request
+    
+    def subdomain_allowed?(allowed_subdomains)
+      allowed_subdomains.any? do |allowed_subdomain|
+        rack_request.host =~ Regexp.new(allowed_subdomain)
+      end
+    end
 
     def path_allowed?(allowed_paths)
       allowed_paths.any? do |allowed_path|
